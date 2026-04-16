784689cd1f02d2560c72c7d4291b6906aa3651a3
Antonyraj <antonyraj22an2005@gmail.com>
2026-04-16 23:09:47 +0530

    Updated project files

---
+++ todoapp/app1/migrations/001 initial.py
@@ -0,0 +1,43 @@
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Teacher',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100)),
+                ('subject', models.CharField(max_length=100)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Classroom',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('room_number', models.CharField(max_length=10)),
+                ('capacity', models.IntegerField()),
+                ('teacher', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='app1.teacher')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Subject',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100)),
+                ('credits', models.IntegerField()),
+            ],
+        ),
+        migrations.AddField(
+            model_name='teacher',
+            name='subjects',
+            field=models.ManyToManyField(to='app1.subject'),
+        ),
+    ]
